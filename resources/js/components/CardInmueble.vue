4a44351af54a75b5ffd2a17404e413b1d05e9186
VMonty24 <luffi.824@gmail.com>
2025-02-25 18:36:18 +0100

    modified:   resources/js/components/CardInmueble.vue 	modified:   resources/js/views/inmuebles/showHomes.vue

--- resources/js/components/CardInmueble.vue
+++ resources/js/components/CardInmueble.vue
@@ -2,11 +2,19 @@
     <div class="card">
         <img class="card-img" src="" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title">Casa en Molins de Rei</h5>
-            <p class="card-text">Estupendisima casa con cocina, salón, comedor, habitaciones, suelo nuevo, paredes e incluso techo y puertas,
-                no te lo pierdas, es una oportunidad única.
-            </p>
-            <h5>340.000</h5>
+            <div class="card-top">
+                <div class="card-content">
+                    <div class="card-text-content">
+                        <h5 class="card-title">Casa en Molins de Rei</h5>
+                        <p class="card-text">Estupendisima casa con cocina, salón, comedor, habitaciones, suelo nuevo, paredes e incluso techo y puertas,
+                            no te lo pierdas, es tupendisima casa con cocina, salón, comedor, habitaciones, suelo nuevo, paredes e incluso techo y puertas,
+                            no te lo pierdas, es una oportunidad únicaEstupendisima casa con cocina, salón, comedor, habitaciones, suelo nuevo, paredes e incluso techo y puertas,
+                            no te lo pierdas, es una oportunidad única</p>  
+                    </div>
+                </div>
+                <h5 class="card-price">340.000</h5>
+            </div>
+            <h6>Inmobiliaria Me quiero ir a casa </h6>
         </div>
     </div>
 </template>
@@ -14,6 +22,11 @@
 <script>
 export default {
     name: 'CardInmueble',
+    methods: {
+        handleButtonClick() {
+            alert('¡Botón clickeado!');
+        }
+    }
 }
 </script>
 
@@ -28,7 +41,28 @@ export default {
     border-radius: 0px !important;
     margin: 0;
     padding: 0;
+    position: relative; /* Para posicionar el círculo */
+}
 
+.card::before {
+    content: "";
+    width: 40px; /* Tamaño del círculo */
+    height: 40px;
+    background-color: white; /* Color de fondo del círculo */
+    border: 1px solid black;
+    border-radius: 50%; /* Para hacerlo circular */
+    position: absolute; /* Para posicionar el círculo */
+    left: 280px; /* Ajusta la posición respecto a la imagen */
+    top: 10%; /* Ajusta la posición respecto a la imagen */
+    z-index: 2; /* Para que esté por encima de la imagen */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzAiIGhlaWdodD0iMzAiIHZpZXdCb3g9InhtbG5zOnZpZGVvL3htbG5zLWFuY2hvcj0ibm9uZSIgeG1sbnM9Imh0dHBzOi8vZGV2ZWxvcGVycy5vcmcvbmlmZXN0eS8yMDEzMDAxIj4KPHBhdGggZD0iTTEuNTg2IDMuMDY4TDQuNTg2IDIuNzA4TDExLjUwNCAxMi4yNTQiIHoiLz4KPC9zdmc+Cg=='); /* Aquí pones tu SVG codificado en Base64 */
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    cursor: pointer; /* Cambiar el cursor a 'pointer' para que se vea como un botón */
 }
 
 .card-img {
@@ -37,12 +71,48 @@ export default {
     background-color: #835EAE;
     object-fit: cover; 
     border-radius: 0px !important;
-    
 }
 
 .card-body {
     display: flex;
     flex-direction: column;
     justify-content: center; 
+    margin: 15px;
+}
+
+.card-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-height: 170px;
+}
+
+.card-content {
+    display: flex;
+    align-items: center;
+    max-width: 600px;
+    width: 100%;
+    max-height: 150px;
+}
+
+.card-text-content {
+    display: flex;
+    flex-direction: column;
+}
+
+.card-price {
+    margin: 30px;
+}
+
+.card-text {
+    max-height: 150px;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 3; /* Número máximo de líneas antes de cortar */
+    line-clamp: 3; /* Número máximo de líneas antes de cortar */
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+    word-break: break-word; /* Asegura que las palabras largas se dividan */
 }
 </style>
--- resources/js/views/inmuebles/showHomes.vue
+++ resources/js/views/inmuebles/showHomes.vue
@@ -1,73 +1,73 @@
 <template>
     <div>
         <h1>Mostrar Inmuebles</h1>
+        <button>Comprar</button>
+        <button>Alquilar</button>
 
+        <div class="container">
+            <div class="filters col-md-3">
+                <h2>Filtros</h2>
+                <div>
+                    <label for="price">Precio</label>
+                    <input type="number" id="price" />
+                </div>
+                <div>
+                    <label for="rooms">Habitaciones</label>
+                    <input type="checkbox" id="rooms" /> 1
+                    <input type="checkbox" id="rooms" />2
+                    <input type="checkbox" id="rooms" />3
+                    <input type="checkbox" id="rooms" />4+
+                </div>
+                <div>
+                    <label for="bathrooms">Baños</label>
+                    <input type="number" id="bathrooms" />
+                </div>
+                <div>
+                    <label for="garage">Garage</label>
+                    <input type="checkbox" id="garage" />
+                </div>
+                <div>
+                    <label for="garden">Jardín</label>
+                    <input type="checkbox" id="garden" />
+                </div>
+                <div>
+                    <label for="pool">Piscina</label>
+                    <input type="checkbox" id="pool" />
+                </div>
+                <div>
+                    <label for="elevator">Ascensor</label>
+                    <input type="checkbox" id="elevator" />
+                </div>
+                <div>
+                    <label for="air-conditioning">Aire acondicionado</label>
+                    <input type="checkbox" id="air-conditioning" />
+                </div>
+                <div>
+                    <label for="heating">Calefacción</label>
+                    <input type="checkbox" id="heating" />
+                </div>
+                <div>
+                    <label for="furnished">Amueblado</label>
+                    <input type="checkbox" id="furnished" />
+                </div>
+                <div>
+                    <label for="security">Seguridad</label>
+                    <input type="checkbox" id="security" />
+                </div>
+                <div>
+                    <label for="new">Obra nueva</label>
+                    <input type="checkbox" id="new" />
+                </div>
+                <div>
+                    <label for="second-hand">Segunda mano</label>
+                    <input type="checkbox" id="second-hand" />
+                </div>
+            </div>
 
-
-        <div class="list-inmuebles">
-            <button>Comprar</button>
-            <button>Alquilar</button>
-            <CardInmuebles />
+            <div class="list-inmuebles col-md-9">
+                <CardInmuebles />
+            </div>
         </div>
-
-        <div class="filters">
-            <h2>Filtros</h2>
-            <div>
-                <label for="price">Precio</label>
-                <input type="number" id="price" />
-            </div>
-            <div>
-                <label for="rooms">Habitaciones</label>
-                <input type="number" id="rooms" />
-            </div>
-            <div>
-                <label for="bathrooms">Baños</label>
-                <input type="number" id="bathrooms" />
-            </div>
-            <div>
-                <label for="garage">Garage</label>
-                <input type="checkbox" id="garage" />
-            </div>
-            <div>
-                <label for="garden">Jardín</label>
-                <input type="checkbox" id="garden" />
-            </div>
-            <div>
-                <label for="pool">Piscina</label>
-                <input type="checkbox" id="pool" />
-            </div>
-            <div>
-                <label for="elevator">Ascensor</label>
-                <input type="checkbox" id="elevator" />
-            </div>
-            <div>
-                <label for="air-conditioning">Aire acondicionado</label>
-                <input type="checkbox" id="air-conditioning" />
-            </div>
-            <div>
-                <label for="heating">Calefacción</label>
-                <input type="checkbox" id="heating" />
-            </div>
-            <div>
-                <label for="furnished">Amueblado</label>
-                <input type="checkbox" id="furnished" />
-            </div>
-            <div>
-                <label for="security">Seguridad</label>
-                <input type="checkbox" id="security" />
-            </div>
-            <div>
-                <label for="new">Obra nueva</label>
-                <input type="checkbox" id="new" />
-            </div>
-            <div>
-                <label for="second-hand">Segunda mano</label>
-                <input type="checkbox" id="second-hand" />
-            </div>
-
-        </div>
-        
-
     </div>
 </template>
 
@@ -83,5 +83,12 @@ export default {
 </script>
 
 <style scoped>
-/* Add your styles here */
+.container {
+    display: flex;
+    margin: 0;
+    padding: 0;
+}
+.filters {
+    margin-right: 20px;
+}
 </style>
