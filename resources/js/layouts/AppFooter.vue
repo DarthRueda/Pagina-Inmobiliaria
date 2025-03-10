f05a4e3b6467f5d66e33db298ef2d56f9edada33
DarthRueda <ruedaar04@gmail.com>
2025-03-10 19:30:32 +0100

    Creado footer y añadido a la pagina.

--- resources/js/layouts/AppFooter.vue
+++ resources/js/layouts/AppFooter.vue
@@ -11,7 +11,72 @@ const logoUrl = computed(() => {
 
 <template>
     <div class="layout-footer">
-        <span class="font-medium ml-2">Ejemplo DAW2</span>
+        <div class="column">
+            <div class="row full-width">
+                <div class="col-4 text-center">
+                    <img src="/images/footer/logo2.svg" alt="Logo">
+                </div>
+                <div class="col-4 text-center">
+                    <span>©2025 Copyright</span>
+                </div>
+                <div class="col-4 text-center">
+                    <img src="/images/footer/facebook.svg" alt="Facebook" class="social-icon">
+                    <img src="/images/footer/instagram.svg" alt="Instagram" class="social-icon">
+                    <img src="/images/footer/youtube.svg" alt="YouTube" class="social-icon">
+                    <img src="/images/footer/link.svg" alt="LinkedIn" class="social-icon">
+                    <img src="/images/footer/tiktok.svg" alt="TikTok" class="social-icon">
+                </div>
+            </div>
+            <div class="row full-width">
+                <div class="col-4 text-center">
+                    <span>Politica de Cookies</span>
+                </div>
+                <div class="col-4 text-center">
+                    <span class="aviso-legal">Aviso Legal</span>
+                </div>
+                <div class="col-4 text-center">
+                    <span>Protecció de dades</span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout-footer {
+    padding: 20px;
+    color: white;
+    background-color: #835EAE;
+}
+.column {
+    display: flex;
+    flex-direction: column;
+    width: 100%; // Ensure the column spans the entire width of the footer
+}
+.row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px; // Add some space between rows
+}
+.full-width {
+    width: 100%; // Ensure the row spans the entire width of the footer
+}
+.col-4 {
+    flex: 0 0 33.3333%;
+    text-align: center; // Center the content of all columns
+}
+.text-center {
+    text-align: center;
+}
+.aviso-legal {
+    margin-top: 0; // Reset margin-top since it's now in a new row
+}
+.social-icon {
+    width: 26px;
+    height: 26px;
+    margin-right: 10px;
+}
+.social-icon:last-child {
+    margin-right: 0;
+}
+</style>
--- resources/js/layouts/Guest.vue
+++ resources/js/layouts/Guest.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-    <Navbar/>
-    <router-view></router-view>
+        <Navbar/>
+        <router-view></router-view>
+        <Footer/>
     </div>
 </template>
+
 <script setup>
 import Navbar from './Nav.vue'
+import Footer from './AppFooter.vue'
 
 </script>
